d35bb1b9524f3a876051a1f359a6d9ff911951fa
Alikper İslam <58908883+alikperislam@users.noreply.github.com>
2022-12-09 03:06:20 +0300

    Update bmp180.py

--- bmp180.py
+++ bmp180.py
@@ -1,30 +1,3 @@
-'''
-bmp180 is a micropython module for the Bosch BMP180 sensor. It measures
-temperature as well as pressure, with a high enough resolution to calculate
-altitude.
-Breakoutboard: http://www.adafruit.com/products/1603  
-data-sheet: http://ae-bst.resource.bosch.com/media/products/dokumente/
-bmp180/BST-BMP180-DS000-09.pdf
-
-The MIT License (MIT)
-Copyright (c) 2014 Sebastian Plamauer, oeplse@gmail.com
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-'''
-
 from ustruct import unpack as unp
 from machine import I2C, Pin
 import math
@@ -36,17 +9,15 @@ class BMP180():
     Module for the BMP180 pressure sensor.
     '''
 
-    _bmp_addr = 119             # adress of BMP180 is hardcoded on the sensor
+    _bmp_addr = 119         
 
     # init
     def __init__(self, i2c_bus):
 
-        # create i2c obect
         _bmp_addr = self._bmp_addr
         self._bmp_i2c = i2c_bus
         #self._bmp_i2c.start()
         self.chip_id = self._bmp_i2c.readfrom_mem(_bmp_addr, 0xD0, 2)
-        # read calibration data from EEPROM
         self._AC1 = unp('>h', self._bmp_i2c.readfrom_mem(_bmp_addr, 0xAA, 2))[0]
         self._AC2 = unp('>h', self._bmp_i2c.readfrom_mem(_bmp_addr, 0xAC, 2))[0]
         self._AC3 = unp('>h', self._bmp_i2c.readfrom_mem(_bmp_addr, 0xAE, 2))[0]
@@ -59,11 +30,9 @@ class BMP180():
         self._MC = unp('>h', self._bmp_i2c.readfrom_mem(_bmp_addr, 0xBC, 2))[0]
         self._MD = unp('>h', self._bmp_i2c.readfrom_mem(_bmp_addr, 0xBE, 2))[0]
 
-        # settings to be adjusted by user
         self.oversample_setting = 3
         self.baseline = 101325.0
 
-        # output raw
         self.UT_raw = None
         self.B5_raw = None
         self.MSB_raw = None
@@ -81,7 +50,6 @@ class BMP180():
         return [self._AC1, self._AC2, self._AC3, self._AC4, self._AC5, self._AC6, 
                 self._B1, self._B2, self._MB, self._MC, self._MD, self.oversample_setting]
 
-    # gauge raw
     def makegauge(self):
         '''
         Generator refreshing the raw measurments.
@@ -90,7 +58,7 @@ class BMP180():
         while True:
             self._bmp_i2c.writeto_mem(self._bmp_addr, 0xF4, bytearray([0x2E]))
             t_start = time.ticks_ms()
-            while (time.ticks_ms() - t_start) <= 5: # 5mS delay
+            while (time.ticks_ms() - t_start) <= 5: 
                 yield None
             try:
                 self.UT_raw = self._bmp_i2c.readfrom_mem(self._bmp_addr, 0xF6, 2)
@@ -148,7 +116,7 @@ class BMP180():
         Pressure in mbar.
         '''
         next(self.gauge)
-        self.temperature  # Populate self.B5_raw
+        self.temperature 
         try:
             MSB = unp('B', self.MSB_raw)[0]
             LSB = unp('B', self.LSB_raw)[0]
